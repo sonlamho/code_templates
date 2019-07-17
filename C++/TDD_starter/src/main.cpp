d45f1f5d6227f60c0c46040ce9fac67249fca8ea
Lam Son Ho <sonlam@math.umd.edu>
2019-07-18 00:16:31 +0700

    add example using third party header

--- C++/TDD_starter/src/main.cpp
+++ C++/TDD_starter/src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "module0.h"
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
 
 int main(){
   printf("Hello, World!\n");
   double a{4.0}, b{5.9};
   printf( "%lf %lf  \n" , squareRoot(a), cubed(b));
+  //
+  json j = R"(
+    {
+      "a":1,
+      "b":[2,3,4,5]
+    }
+  )"_json;
+  std::cout << j << '\n';
+
   return 1;
 }
