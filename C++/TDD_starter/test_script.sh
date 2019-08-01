f4c20b28fa02609a2cc64e4009bda48875d5da11
Lam Son Ho <sonlam@math.umd.edu>
2019-08-01 22:34:06 +0700

    export compile_commands.json, make link to it from project root folder

--- C++/TDD_starter/test_script.sh
+++ C++/TDD_starter/test_script.sh
@@ -1,8 +1,10 @@
 #test_script
 mkdir -p build
 cd build
-cmake ..
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 if [[ $? -eq 0 ]]; then
+    rm -f ../compile_commands.json
+    ln -s ./build/compile_commands.json ../compile_commands.json
     make
 fi
 if [[ $? -eq 0 ]]; then
