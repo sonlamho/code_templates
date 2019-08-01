6f306dfe6d91b5737b551ffe6104d987517cf1f8
Lam Son Ho <sonlam@math.umd.edu>
2019-08-01 23:25:47 +0700

    set clearer boundary in CMakeLists.txt

--- C++/TDD_starter/test_script.sh
+++ C++/TDD_starter/test_script.sh
@@ -5,7 +5,7 @@ cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 if [[ $? -eq 0 ]]; then
     rm -f ../compile_commands.json
     ln -s ./build/compile_commands.json ../compile_commands.json
-    make
+    cmake --build .
 fi
 if [[ $? -eq 0 ]]; then
     ./runTests
