66b70d45048fddf24cb92be81157f1fed7c6ff69
Lam Son Ho <sonlam@math.umd.edu>
2019-08-02 12:05:51 +0700

    make building tests optional

--- C++/TDD_starter/test_script.sh
+++ C++/TDD_starter/test_script.sh
@@ -1,12 +1,12 @@
 #test_script
 mkdir -p build
 cd build
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DBUILD_TESTS=1
 if [[ $? -eq 0 ]]; then
     rm -f ../compile_commands.json
     ln -s ./build/compile_commands.json ../compile_commands.json
     cmake --build .
 fi
 if [[ $? -eq 0 ]]; then
-    ./runTests
+    ./tests/runTests
 fi
