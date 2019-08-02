8c2e5d19c1484c5670fe88ad6aa2c721b0369ab3
Lam Son Ho <sonlam@math.umd.edu>
2019-08-02 15:28:56 +0700

    comments

--- C++/TDD_starter/test_script.sh
+++ C++/TDD_starter/test_script.sh
@@ -3,8 +3,11 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DBUILD_TESTS=1
 if [[ $? -eq 0 ]]; then
+    # link compile_commands.json to root folder for `clangd`
     rm -f ../compile_commands.json
     ln -s ./build/compile_commands.json ../compile_commands.json
+
+    # build
     cmake --build .
 fi
 if [[ $? -eq 0 ]]; then
